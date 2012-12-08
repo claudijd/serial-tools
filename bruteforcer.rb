da8d6e86c3033738cbca137b05433b28bd7393de
Jonathan Claudius <claudijd@yahoo.com>
2012-12-08 16:09:25 -0600

    Porting old bruteforcer code to use select method
    This helps in many ways to show you data output, even if it's not
    necessarily understandable, it does help you understand that you are
    getting some form of data transfer and that you're pretty close.

--- bruteforcer.rb
+++ bruteforcer.rb
@@ -2,7 +2,7 @@
 
 require "serialport"
 
-port_str = "/dev/tty.usbserial-A100FY8Y"
+port_str = ARGV.shift #"/dev/tty.usbserial-A5005DKM"
 baud_rates = [
   9600,
   38400,
@@ -19,7 +19,9 @@ loop do
   baud_rates.each do |baud_rate|
     puts "\n*********** START BAUD #{baud_rate} ***********\n"
     sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
-    reader = Thread.new { loop { printf("%c", sp.getc) } }
+    $stdout.sync= true
+    sp.sync = true
+    reader = Thread.new { loop { IO.select([sp]); $stdout.write(sp.read_nonblock(4096)); } }
     writer = Thread.new { loop { sp.write(gets) } }
     timer = Thread.new { sleep(timeout) }
 
